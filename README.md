78eeca07fcafb90d5beadab54f21bf5a05c490e2
asuratu <455224159@qq.com>
2023-08-21 09:04:29 +0800

    初始化

---
+++ README.md
@@ -0,0 +1,8 @@
+# Laravel Skeleton
+
+Laravel 项目快速开发骨架
+- 常用包的自动化配置
+
+框架要求
+- Laravel >= 9
+- php >= 8.0
---
+++ git.sh
@@ -0,0 +1,10 @@
+CONTENT=$1
+
+# CONTENT 默认值为 "update"
+if [ -z "$CONTENT" ]; then
+    CONTENT="update"
+fi
+
+git add .
+git commit -m "$CONTENT"
+#git push
